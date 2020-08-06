0178dadb7925d3986acaced949f3240b68d21eb3
moda <69039425+modafinilici@users.noreply.github.com>
2020-08-06 06:50:44 +0300

    Rolled back tailwind.config.js with the purge options

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,4 @@
 module.exports = {
- purge: [
-    "./pages/**/*.vue", 
-    "./components/**/*.vue", 
-    "./plugins/**/*.vue",
-    "./static/**/*.vue",
-    "./store/**/*.vue"
-  ],
   theme: {
     extend: {
       colors: {
