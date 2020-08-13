554ff8652c2d4db19127bea3bf6047ba3f6253ea
moda <69039425+modafinilici@users.noreply.github.com>
2020-08-14 01:53:18 +0300

    Added accordion on the homepage, updated TitleTemplate by removing the - Sitename from being added automatically on the metatitle

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -144,21 +144,7 @@ export default {
       ]
     }
   }
-};
-
-var myRadios = document.getElementsByName('faq');
-      var setCheck;
-      var x = 0;
-      for(x = 0; x < myRadios.length; x++){
-          myRadios[x].onclick = function(){
-              if(setCheck != this){
-                   setCheck = this;
-              }else{
-                  this.checked = false;
-                  setCheck = null;
-          }
-          };
-      }
+}
 </script>
 
 <style>
