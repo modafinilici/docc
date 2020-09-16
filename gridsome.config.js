877a1ef03c7934b675907ca5ea660da9550070df
moda <69039425+modafinilici@users.noreply.github.com>
2020-09-16 23:53:10 +0300

    Added PurgeCSS Gridsome Plugin

--- gridsome.config.js
+++ gridsome.config.js
@@ -73,6 +73,21 @@ module.exports = {
     },
 
     {
+      use: 'gridsome-plugin-purgecss',
+      // default options, the following will be included if you don't provide anything
+      options: {
+        content: [
+          './src/**/*.vue',
+          './src/**/*.js',
+          './src/**/*.jsx',
+          './src/**/*.pug',
+          './src/**/*.md'
+        ],
+        defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+      }
+    },
+
+    {
       use: '@gridsome/plugin-google-analytics',
       options: {
         id: (process.env.GA_ID ? process.env.GA_ID : 'UA-144741960-1')
